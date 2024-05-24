c3fad1a184ad741cbfb3ad49378eea369771b864
LihuelZerega <zeregalihuel@gmail.com>
2024-05-24 14:54:38 -0300

    Home fix

--- src/modules/home/components/section-products/index.tsx
+++ src/modules/home/components/section-products/index.tsx
@@ -4,8 +4,8 @@ import { Image } from "@nextui-org/react"
 function index() {
   return (
     <div className="px-6 2xl:px-64 py-12">
-      <section className="grid grid-cols-1 lg:grid-cols-2 items-center justify-center gap-x-6">
-        <div className="relative lg:h-[500px] 2xl:h-[600px] w-full bg-neutral-100 border-1 border-neutral-200 rounded-lg">
+      <section className="grid grid-cols-1 lg:grid-cols-2 items-center justify-center gap-y-6 lg:gap-x-6">
+        <div className="relative lg:h-[500px] 2xl:h-[600px] w-full rounded-lg">
           <div className="absolute inset-0 flex flex-col items-center justify-center z-10 select-none">
             <h2 className="font-semibold text-xl pb-2">NEW</h2>
             <a
@@ -26,7 +26,7 @@ function index() {
           />
         </div>
 
-        <div className="relative lg:h-[500px] 2xl:h-[600px] w-full bg-neutral-100 border-1 border-neutral-200 rounded-lg">
+        <div className="relative lg:h-[500px] 2xl:h-[600px] w-full rounded-lg">
           <div className="absolute inset-0 flex flex-col items-center justify-center z-10 select-none">
             <h2 className="font-semibold text-xl text-neutral-200 pb-2">
               SALE
@@ -50,8 +50,8 @@ function index() {
         </div>
       </section>
 
-      <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 items-center justify-center xl:pt-64 2xl:pt-32 pb-12 gap-x-6">
-        <a href="" className="relative flex flex-col items-center justify-center font-semibold pt-2 lg:h-[250px] 2xl:h-[300px] w-full bg-neutral-100 border-1 border-neutral-200 rounded-lg cursor-pointer">
+      <section className="grid grid-cols-2 xl:grid-cols-4 items-center justify-center pt-12 xl:pt-64 2xl:pt-32 pb-12 gap-6 lg:gap-x-6">
+        <a href="" className="relative flex flex-col items-center justify-center font-semibold pt-2 lg:h-[250px] 2xl:h-[300px] w-full rounded-lg cursor-pointer">
           <h2 className="absolute z-10 select-none text-neutral-200">SALE</h2>
           <Image
             isZoomed
@@ -63,7 +63,7 @@ function index() {
             className="z-0"
           />
         </a>
-        <a href="" className="relative flex flex-col items-center justify-center font-semibold pt-2 lg:h-[250px] 2xl:h-[300px] w-full bg-neutral-100 border-1 border-neutral-200 rounded-lg cursor-pointer">
+        <a href="" className="relative flex flex-col items-center justify-center font-semibold pt-2 lg:h-[250px] 2xl:h-[300px] w-full rounded-lg cursor-pointer">
           <h2 className="absolute z-10 select-none">HOODIES</h2>
           <Image
             isZoomed
@@ -75,7 +75,7 @@ function index() {
             className="z-0"
           />
         </a>
-        <a href="" className="relative flex flex-col items-center justify-center font-semibold pt-2 lg:h-[250px] 2xl:h-[300px] w-full bg-neutral-100 border-1 border-neutral-200 rounded-lg cursor-pointer">
+        <a href="" className="relative flex flex-col items-center justify-center font-semibold pt-2 lg:h-[250px] 2xl:h-[300px] w-full rounded-lg cursor-pointer">
           <h2 className="absolute text-neutral-200 z-10 select-none">PANTS</h2>
           <Image
             isZoomed
@@ -87,7 +87,7 @@ function index() {
             className="z-0"
           />
         </a>
-        <a href="" className="relative flex flex-col items-center justify-center font-semibold pt-2 lg:h-[250px] 2xl:h-[300px] w-full bg-neutral-100 border-1 border-neutral-200 rounded-lg cursor-pointer">
+        <a href="" className="relative flex flex-col items-center justify-center font-semibold pt-2 lg:h-[250px] 2xl:h-[300px] w-full rounded-lg cursor-pointer">
           <h2 className="absolute z-10 select-none">T-SHIRTS</h2>
           <Image
             isZoomed
